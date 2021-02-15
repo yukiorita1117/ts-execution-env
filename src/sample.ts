e5ab65763ae87ce8b5c256f3a9ff513185b10552
折田幸夫 <3204704@gmail.com>
2021-02-15 19:26:44 +0900

    型注釈

--- src/sample.ts
+++ src/sample.ts
@@ -1,3 +1,8 @@
 const personName: string = "jhcoder";
 
 console.log(`Hello ${personName}!`);
+
+// 引数や返値に型注釈（アノテーション）を必ずつけるべき
+export function Person(name: string): string {
+  return `Hello ${name}`;
+}

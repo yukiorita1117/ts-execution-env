c3d5792304120e3119179d79d9c21f8f38a9f775
折田幸夫 <3204704@gmail.com>
2021-02-16 01:46:38 +0900

    feat Union

--- src/sample.ts
+++ src/sample.ts
@@ -6,3 +6,28 @@ console.log(`Hello ${personName}!`);
 export function Person(name: string): string {
   return `Hello ${name}`;
 }
+
+export function Tuple1() {
+  const tmpArray: [string, number] = ["あああ", 10];
+  return tmpArray;
+}
+
+export function Tuple2() {
+  const tmpArray: [string, number] = ["あああ", 10];
+  console.log("テスト", tmpArray[0].substr(1)); // ああ
+  console.log("テスト", tmpArray[0].substr(2)); // あ
+  // console.log("テスト", tmpArray[1].substr(2)); //この場合number型にsubstr()は使えないためerrorとなる
+
+  return tmpArray;
+}
+
+// 以下のようなstringとnumberの混じった配列の方はUnion型で定義するといける
+export function Union1() {
+  let tmpArray: (string | number)[];
+  tmpArray = [1, 2, 4, "あああ", 10, "ろく"];
+  console.log("ユニオン", tmpArray[3].toString());
+  return tmpArray;
+}
+
+Tuple2();
+Union1();

e1811caf4b8d003f309b26c33121927b58875a90
折田幸夫 <3204704@gmail.com>
2021-02-21 19:57:08 +0900

    fix

--- src/assersion.ts
+++ src/assersion.ts
@@ -3,12 +3,13 @@
 
 // 一つ目のアサーション構文
 // someValueはstring型だが、そのlength はnumber型だよっとアサーションしている。
-// JSXに置いてこの＜＞を用いたアサーションは非推奨。だけど割と使われている。
+// JSXにおいてこの＜＞を用いたアサーションは非推奨。だけど割と使われている。
 const someValue: string = "これは文字列です";
 let stringLength: number = (<string>someValue).length;
+const tmp = someValue.length;
 // TODO 何で string lengthが標準出力できない？
-console.log("ああああ", someValue.length);
-console.log("assersion : ", stringLength);
+console.log("ああああ", String(tmp));
+console.log("assersion : ", String(stringLength));
 
 let someValue2: any = "これは文字列です";
 let stringLength2: number = someValue2.length;
